834ed1e73740720f097b5d7853bfbbf5ff0de66b
1 <1>
2017-11-02 11:40:43 +0200

    1

--- web-bluetooth/characteristic-properties.js
+++ web-bluetooth/characteristic-properties.js
@@ -23,15 +23,28 @@ function onButtonClick() {
     return server.getPrimaryService(serviceUuid);
   })
   .then(service => {
-    log('Getting Chafsafasfracteristic...');
+
     return service.getCharacteristic(characteristicUuid);
   })
   .then(characteristic => {
+        log('Getting Chafsafasfracteristic...');
+    log('> Characteristic UUID:  ' + characteristic.uuid);
+    log('> Broadcast:            ' + characteristic.properties.broadcast);
+    log('> Read:                 ' + characteristic.properties.read);
+    log('> Write w/o response:   ' +
+        characteristic.properties.writeWithoutResponse);
+    log('> Write:                ' + characteristic.properties.write);
+    log('> Notify:               ' + characteristic.properties.notify);
+    log('> Indicate:             ' + characteristic.properties.indicate);
+    log('> Signed Write:         ' +
+        characteristic.properties.authenticatedSignedWrites);
+    log('> Queued Write:         ' + characteristic.properties.reliableWrite);
+    log('> Writable Auxiliaries: ' + characteristic.properties.writableAuxiliaries);
     return characteristic.readValue();
   })
  .then(value => {
   console.log('value ' + value.getUint8(0));
-})
+    })
   .catch(error => {
     log('Argh! ' + error);
   });
